22bdba691ccf763cf1af3c60c2a48a4068c99a32
fisherjrs <fisher.j.r.s@gmail.com>
2014-11-28 09:08:34 -0600

    update date sample

--- date_sample.sh
+++ date_sample.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 
+#%m is month
+#%M is minute
+#%D is date
+#%d is day
+#%Y is 4 digit year
+#%y is 2 digit year
+#%T is time hh:mm:ss
+#%t nothing
+#%s seconds since epoch
+#%Y-%m-%d %T - regular timestamp
+
+
 #two functions: timestamp and datestamp
 timestamp() {
 	date +"%T"
@@ -8,6 +20,8 @@ timestamp() {
 dateTimeStamp() {
 	date +"%Y-%m-%d %T"
 }
+
+echo $(date +"%Y-%m-%d %T")
 
 timestamp1=$(date +%s)
 echo "seconds since epoch :: $timestamp1"
